ee2e63305109ea796bc76ab399a85423acc81bb9
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2022-04-02 12:11:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $token = str_replace('"', '', $_GET['auth']);
 $tokenold = $token;
 $token = md5($token);
 if ($token == "9e78c5c20b172e66f75779d35040796a" or $token == "d2555ef8faa2788ebb5434b6dc9955cd"){
-	$ip = $_GET['ip']
+	$ip = $_GET['ip'];
 
 	if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
 	  	$command = "/app/nmap/bin/nmap -v" . $ip . "2>&1";
@@ -19,7 +19,7 @@ if ($token == "9e78c5c20b172e66f75779d35040796a" or $token == "d2555ef8faa2788eb
 		}
 	}
 	else {
-	  	echo "Not valid ip address."
+	  	echo "Not valid ip address.";
 	}
 }else{
   printl("Error! incorrect token");  
