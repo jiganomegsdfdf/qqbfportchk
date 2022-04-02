842f271f1a47bfdaeaac6da9cb29daba8a24e473
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2022-04-02 12:19:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,16 +8,10 @@ if ($token == "9e78c5c20b172e66f75779d35040796a" or $token == "d2555ef8faa2788eb
 
 	if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
 	  	$command = "/app/nmap/bin/nmap -v " . $ip . " 2>&1";
-		print($command);
+		//print($command);
 
 		exec($command, $output, $return_var);
-	
-		// If return code is not zero, the command failed
-		if ($return_var != 0) 
-		{
- 		   // dump all output, including error messages
-		    var_dump($output);
-		}
+		var_dump($output);
 	}
 	else {
 	  	echo "Not valid ip address.";
