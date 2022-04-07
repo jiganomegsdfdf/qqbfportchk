3a34573281f17808ad2051e07435bca4f59af70d
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2022-04-07 17:27:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,8 +21,9 @@ function checker($from, $to, $ip) {
 		array_shift($textarr);
 		array_shift($textarr);
 		$str = array_shift($textarr);
-		$pings = explode("s elapsed",explode("", "", $str)[1])[0];
-		$pingms = explode("s elapsed",explode("", "", $str)[1])[0]*1000;
+		var_dump($str);
+		$pings = explode("s elapsed",explode(', ', $str)[1])[0];
+		$pingms = explode("s elapsed",explode(', ', $str)[1])[0]*1000;
 
 		array_shift($textarr);
 		array_shift($textarr);
