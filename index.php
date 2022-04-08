54ddfb00b207d56e82036c0536c47a0819b9ddf0
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2022-04-08 17:00:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,47 +4,261 @@ print("<head>");
 print("		<link rel='stylesheet' href='s.css'>");
 print("</head>");
 
-function checker($from, $to, $ip) {
-		$command = "/app/nmap/bin/nmap --version-intensity 0 -v -p " . $from . "-" . $to . " -sT " . $ip . " 2>&1";
+function code_to_country( $code ){
 
-		exec($command, $output, $return_var);
-		//var_dump($output);
-		$textarr = $output;
-		$portarr = array("");
-		array_shift($portarr);
-		array_shift($textarr);
-		array_shift($portarr);
-		array_shift($textarr);
-		array_shift($textarr);
-		array_shift($textarr);
-		array_shift($textarr);
-		array_shift($textarr);
-		$str = array_shift($textarr);
-		$pings = explode("s elapsed",explode(', ', $str)[1])[0];
-		$pingms = explode("s elapsed",explode(', ', $str)[1])[0]*1000;
+    $code = strtoupper($code);
 
-		array_shift($textarr);
-		array_shift($textarr);
-		array_shift($textarr);
-		array_shift($textarr);
-		$portcount = 0;
-		$preportsarr = $textarr;
-		while (true){
-			if (count($preportsarr) > 1){
-				$str = array_shift($preportsarr);
-				if (str_contains($str, "Discovered")){
-					$portcount += 1;
-					array_push($portarr, $str);
-					array_shift($textarr);
-				}else{
-					break;
-				}
-			}else{
-				break;
-			}
-		}
-		$all = array($pings, $pingms, $portcount, $portarr);
-		return $all;
+    $countryList = array(
+        'AF' => 'Afghanistan',
+        'AX' => 'Aland Islands',
+        'AL' => 'Albania',
+        'DZ' => 'Algeria',
+        'AS' => 'American Samoa',
+        'AD' => 'Andorra',
+        'AO' => 'Angola',
+        'AI' => 'Anguilla',
+        'AQ' => 'Antarctica',
+        'AG' => 'Antigua and Barbuda',
+        'AR' => 'Argentina',
+        'AM' => 'Armenia',
+        'AW' => 'Aruba',
+        'AU' => 'Australia',
+        'AT' => 'Austria',
+        'AZ' => 'Azerbaijan',
+        'BS' => 'Bahamas the',
+        'BH' => 'Bahrain',
+        'BD' => 'Bangladesh',
+        'BB' => 'Barbados',
+        'BY' => 'Belarus',
+        'BE' => 'Belgium',
+        'BZ' => 'Belize',
+        'BJ' => 'Benin',
+        'BM' => 'Bermuda',
+        'BT' => 'Bhutan',
+        'BO' => 'Bolivia',
+        'BA' => 'Bosnia and Herzegovina',
+        'BW' => 'Botswana',
+        'BV' => 'Bouvet Island (Bouvetoya)',
+        'BR' => 'Brazil',
+        'IO' => 'British Indian Ocean Territory (Chagos Archipelago)',
+        'VG' => 'British Virgin Islands',
+        'BN' => 'Brunei Darussalam',
+        'BG' => 'Bulgaria',
+        'BF' => 'Burkina Faso',
+        'BI' => 'Burundi',
+        'KH' => 'Cambodia',
+        'CM' => 'Cameroon',
+        'CA' => 'Canada',
+        'CV' => 'Cape Verde',
+        'KY' => 'Cayman Islands',
+        'CF' => 'Central African Republic',
+        'TD' => 'Chad',
+        'CL' => 'Chile',
+        'CN' => 'China',
+        'CX' => 'Christmas Island',
+        'CC' => 'Cocos (Keeling) Islands',
+        'CO' => 'Colombia',
+        'KM' => 'Comoros the',
+        'CD' => 'Congo',
+        'CG' => 'Congo the',
+        'CK' => 'Cook Islands',
+        'CR' => 'Costa Rica',
+        'CI' => 'Cote d\'Ivoire',
+        'HR' => 'Croatia',
+        'CU' => 'Cuba',
+        'CY' => 'Cyprus',
+        'CZ' => 'Czech Republic',
+        'DK' => 'Denmark',
+        'DJ' => 'Djibouti',
+        'DM' => 'Dominica',
+        'DO' => 'Dominican Republic',
+        'EC' => 'Ecuador',
+        'EG' => 'Egypt',
+        'SV' => 'El Salvador',
+        'GQ' => 'Equatorial Guinea',
+        'ER' => 'Eritrea',
+        'EE' => 'Estonia',
+        'ET' => 'Ethiopia',
+        'FO' => 'Faroe Islands',
+        'FK' => 'Falkland Islands (Malvinas)',
+        'FJ' => 'Fiji the Fiji Islands',
+        'FI' => 'Finland',
+        'FR' => 'France, French Republic',
+        'GF' => 'French Guiana',
+        'PF' => 'French Polynesia',
+        'TF' => 'French Southern Territories',
+        'GA' => 'Gabon',
+        'GM' => 'Gambia the',
+        'GE' => 'Georgia',
+        'DE' => 'Germany',
+        'GH' => 'Ghana',
+        'GI' => 'Gibraltar',
+        'GR' => 'Greece',
+        'GL' => 'Greenland',
+        'GD' => 'Grenada',
+        'GP' => 'Guadeloupe',
+        'GU' => 'Guam',
+        'GT' => 'Guatemala',
+        'GG' => 'Guernsey',
+        'GN' => 'Guinea',
+        'GW' => 'Guinea-Bissau',
+        'GY' => 'Guyana',
+        'HT' => 'Haiti',
+        'HM' => 'Heard Island and McDonald Islands',
+        'VA' => 'Holy See (Vatican City State)',
+        'HN' => 'Honduras',
+        'HK' => 'Hong Kong',
+        'HU' => 'Hungary',
+        'IS' => 'Iceland',
+        'IN' => 'India',
+        'ID' => 'Indonesia',
+        'IR' => 'Iran',
+        'IQ' => 'Iraq',
+        'IE' => 'Ireland',
+        'IM' => 'Isle of Man',
+        'IL' => 'Israel',
+        'IT' => 'Italy',
+        'JM' => 'Jamaica',
+        'JP' => 'Japan',
+        'JE' => 'Jersey',
+        'JO' => 'Jordan',
+        'KZ' => 'Kazakhstan',
+        'KE' => 'Kenya',
+        'KI' => 'Kiribati',
+        'KP' => 'Korea',
+        'KR' => 'Korea',
+        'KW' => 'Kuwait',
+        'KG' => 'Kyrgyz Republic',
+        'LA' => 'Lao',
+        'LV' => 'Latvia',
+        'LB' => 'Lebanon',
+        'LS' => 'Lesotho',
+        'LR' => 'Liberia',
+        'LY' => 'Libyan Arab Jamahiriya',
+        'LI' => 'Liechtenstein',
+        'LT' => 'Lithuania',
+        'LU' => 'Luxembourg',
+        'MO' => 'Macao',
+        'MK' => 'Macedonia',
+        'MG' => 'Madagascar',
+        'MW' => 'Malawi',
+        'MY' => 'Malaysia',
+        'MV' => 'Maldives',
+        'ML' => 'Mali',
+        'MT' => 'Malta',
+        'MH' => 'Marshall Islands',
+        'MQ' => 'Martinique',
+        'MR' => 'Mauritania',
+        'MU' => 'Mauritius',
+        'YT' => 'Mayotte',
+        'MX' => 'Mexico',
+        'FM' => 'Micronesia',
+        'MD' => 'Moldova',
+        'MC' => 'Monaco',
+        'MN' => 'Mongolia',
+        'ME' => 'Montenegro',
+        'MS' => 'Montserrat',
+        'MA' => 'Morocco',
+        'MZ' => 'Mozambique',
+        'MM' => 'Myanmar',
+        'NA' => 'Namibia',
+        'NR' => 'Nauru',
+        'NP' => 'Nepal',
+        'AN' => 'Netherlands Antilles',
+        'NL' => 'Netherlands the',
+        'NC' => 'New Caledonia',
+        'NZ' => 'New Zealand',
+        'NI' => 'Nicaragua',
+        'NE' => 'Niger',
+        'NG' => 'Nigeria',
+        'NU' => 'Niue',
+        'NF' => 'Norfolk Island',
+        'MP' => 'Northern Mariana Islands',
+        'NO' => 'Norway',
+        'OM' => 'Oman',
+        'PK' => 'Pakistan',
+        'PW' => 'Palau',
+        'PS' => 'Palestinian Territory',
+        'PA' => 'Panama',
+        'PG' => 'Papua New Guinea',
+        'PY' => 'Paraguay',
+        'PE' => 'Peru',
+        'PH' => 'Philippines',
+        'PN' => 'Pitcairn Islands',
+        'PL' => 'Poland',
+        'PT' => 'Portugal, Portuguese Republic',
+        'PR' => 'Puerto Rico',
+        'QA' => 'Qatar',
+        'RE' => 'Reunion',
+        'RO' => 'Romania',
+        'RU' => 'Russian Federation',
+        'RW' => 'Rwanda',
+        'BL' => 'Saint Barthelemy',
+        'SH' => 'Saint Helena',
+        'KN' => 'Saint Kitts and Nevis',
+        'LC' => 'Saint Lucia',
+        'MF' => 'Saint Martin',
+        'PM' => 'Saint Pierre and Miquelon',
+        'VC' => 'Saint Vincent and the Grenadines',
+        'WS' => 'Samoa',
+        'SM' => 'San Marino',
+        'ST' => 'Sao Tome and Principe',
+        'SA' => 'Saudi Arabia',
+        'SN' => 'Senegal',
+        'RS' => 'Serbia',
+        'SC' => 'Seychelles',
+        'SL' => 'Sierra Leone',
+        'SG' => 'Singapore',
+        'SK' => 'Slovakia (Slovak Republic)',
+        'SI' => 'Slovenia',
+        'SB' => 'Solomon Islands',
+        'SO' => 'Somalia, Somali Republic',
+        'ZA' => 'South Africa',
+        'GS' => 'South Georgia and the South Sandwich Islands',
+        'ES' => 'Spain',
+        'LK' => 'Sri Lanka',
+        'SD' => 'Sudan',
+        'SR' => 'Suriname',
+        'SJ' => 'Svalbard & Jan Mayen Islands',
+        'SZ' => 'Swaziland',
+        'SE' => 'Sweden',
+        'CH' => 'Switzerland, Swiss Confederation',
+        'SY' => 'Syrian Arab Republic',
+        'TW' => 'Taiwan',
+        'TJ' => 'Tajikistan',
+        'TZ' => 'Tanzania',
+        'TH' => 'Thailand',
+        'TL' => 'Timor-Leste',
+        'TG' => 'Togo',
+        'TK' => 'Tokelau',
+        'TO' => 'Tonga',
+        'TT' => 'Trinidad and Tobago',
+        'TN' => 'Tunisia',
+        'TR' => 'Turkey',
+        'TM' => 'Turkmenistan',
+        'TC' => 'Turks and Caicos Islands',
+        'TV' => 'Tuvalu',
+        'UG' => 'Uganda',
+        'UA' => 'Ukraine',
+        'AE' => 'United Arab Emirates',
+        'GB' => 'United Kingdom',
+        'US' => 'United States of America',
+        'UM' => 'United States Minor Outlying Islands',
+        'VI' => 'United States Virgin Islands',
+        'UY' => 'Uruguay, Eastern Republic of',
+        'UZ' => 'Uzbekistan',
+        'VU' => 'Vanuatu',
+        'VE' => 'Venezuela',
+        'VN' => 'Vietnam',
+        'WF' => 'Wallis and Futuna',
+        'EH' => 'Western Sahara',
+        'YE' => 'Yemen',
+        'ZM' => 'Zambia',
+        'ZW' => 'Zimbabwe'
+    );
+
+    if( !$countryList[$code] ) return $code;
+    else return $countryList[$code];
 }
 
 $token = str_replace('"', '', $_GET['auth']);
@@ -54,73 +268,6 @@ if ($token == "9e78c5c20b172e66f75779d35040796a" or $token == "d2555ef8faa2788eb
 	$ip = $_GET['ip'];
 
 	if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-		/*list($pings1, $pingms1, $portcount1, $portarr1) = checker(0,1000, $ip);
-		list($pings2, $pingms2, $portcount2, $portarr2) = checker(1001,2000, $ip);
-		list($pings3, $pingms3, $portcount3, $portarr3) = checker(2001,3000, $ip);
-		list($pings4, $pingms4, $portcount4, $portarr4) = checker(3001,4000, $ip);
-		list($pings5, $pingms5, $portcount5, $portarr5) = checker(4001,5000, $ip);
-		list($pings6, $pingms6, $portcount6, $portarr6) = checker(5001,6000, $ip);
-		list($pings7, $pingms7, $portcount7, $portarr7) = checker(6001,7000, $ip);
-		list($pings8, $pingms8, $portcount8, $portarr8) = checker(7001,8000, $ip);
-		list($pings9, $pingms9, $portcount9, $portarr9) = checker(8001,9000, $ip);
-		list($pings10, $pingms10, $portcount10, $portarr10) = checker(9001,10000, $ip);
-		
-		$portcount = $portcount1 + $portcount2 + $portcount3 + $portcount4 + $portcount5 + $portcount6 + $portcount7 + $portcount8 + $portcount9 + $portcount10;  
-		$portarr = array_merge($portarr1, $portarr2, $portarr3, $portarr4, $portarr5, $portarr6, $portarr7, $portarr8, $portarr9, $portarr10);
-		
-		list($pings1, $pingms1, $portcount1, $portarr1) = checker(10000+1,10000+1000, $ip);
-		list($pings2, $pingms2, $portcount2, $portarr2) = checker(10000+1001,10000+2000, $ip);
-		list($pings3, $pingms3, $portcount3, $portarr3) = checker(10000+2001,10000+3000, $ip);
-		list($pings4, $pingms4, $portcount4, $portarr4) = checker(10000+3001,10000+4000, $ip);
-		list($pings5, $pingms5, $portcount5, $portarr5) = checker(10000+4001,10000+5000, $ip);
-		list($pings6, $pingms6, $portcount6, $portarr6) = checker(10000+5001,10000+6000, $ip);
-		list($pings7, $pingms7, $portcount7, $portarr7) = checker(10000+6001,10000+7000, $ip);
-		list($pings8, $pingms8, $portcount8, $portarr8) = checker(10000+7001,10000+8000, $ip);
-		list($pings9, $pingms9, $portcount9, $portarr9) = checker(10000+8001,10000+9000, $ip);
-		list($pings10, $pingms10, $portcount10, $portarr10) = checker(10000+9001,10000+10000, $ip);
-		
-		$portcount = $portcount1 + $portcount2 + $portcount3 + $portcount4 + $portcount5 + $portcount6 + $portcount7 + $portcount8 + $portcount9 + $portcount10 + $portcount;
-		$portarr = array_merge($portarr, $portarr1, $portarr2, $portarr3, $portarr4, $portarr5, $portarr6, $portarr7, $portarr8, $portarr9, $portarr10); 
-		
-		list($pings1, $pingms1, $portcount1, $portarr1) = checker(20000+1,20000+1000, $ip);
-		list($pings2, $pingms2, $portcount2, $portarr2) = checker(20000+1001,20000+2000, $ip);
-		list($pings3, $pingms3, $portcount3, $portarr3) = checker(20000+2001,20000+3000, $ip);
-		list($pings4, $pingms4, $portcount4, $portarr4) = checker(20000+3001,20000+4000, $ip);
-		list($pings5, $pingms5, $portcount5, $portarr5) = checker(20000+4001,20000+5000, $ip);
-		list($pings6, $pingms6, $portcount6, $portarr6) = checker(20000+5001,20000+6000, $ip);
-		list($pings7, $pingms7, $portcount7, $portarr7) = checker(20000+6001,20000+7000, $ip);
-		list($pings8, $pingms8, $portcount8, $portarr8) = checker(20000+7001,20000+8000, $ip);
-		list($pings9, $pingms9, $portcount9, $portarr9) = checker(20000+8001,20000+9000, $ip);
-		list($pings10, $pingms10, $portcount10, $portarr10) = checker(20000+9001,20000+10000, $ip);
-		
-		$portcount = $portcount1 + $portcount2 + $portcount3 + $portcount4 + $portcount5 + $portcount6 + $portcount7 + $portcount8 + $portcount9 + $portcount10 + $portcount;
-		$portarr = array_merge($portarr, $portarr1, $portarr2, $portarr3, $portarr4, $portarr5, $portarr6, $portarr7, $portarr8, $portarr9, $portarr10); 
-		
-		list($pings1, $pingms1, $portcount1, $portarr1) = checker(30000+1,30000+1000, $ip);
-		list($pings2, $pingms2, $portcount2, $portarr2) = checker(30000+1001,30000+2000, $ip);
-		list($pings3, $pingms3, $portcount3, $portarr3) = checker(30000+2001,30000+3000, $ip);
-		list($pings4, $pingms4, $portcount4, $portarr4) = checker(30000+3001,30000+4000, $ip);
-		list($pings5, $pingms5, $portcount5, $portarr5) = checker(30000+4001,30000+5000, $ip);
-		list($pings6, $pingms6, $portcount6, $portarr6) = checker(30000+5001,30000+6000, $ip);
-		list($pings7, $pingms7, $portcount7, $portarr7) = checker(30000+6001,30000+7000, $ip);
-		list($pings8, $pingms8, $portcount8, $portarr8) = checker(30000+7001,30000+8000, $ip);
-		list($pings9, $pingms9, $portcount9, $portarr9) = checker(30000+8001,30000+9000, $ip);
-		list($pings10, $pingms10, $portcount10, $portarr10) = checker(30000+9001,30000+10000, $ip);
-		
-		$portcount = $portcount1 + $portcount2 + $portcount3 + $portcount4 + $portcount5 + $portcount6 + $portcount7 + $portcount8 + $portcount9 + $portcount10 + $portcount;
-		$portarr = array_merge($portarr, $portarr1, $portarr2, $portarr3, $portarr4, $portarr5, $portarr6, $portarr7, $portarr8, $portarr9, $portarr10); 
-					
-		list($pings1, $pingms1, $portcount1, $portarr1) = checker(40000+1,40000+1000, $ip);
-		list($pings2, $pingms2, $portcount2, $portarr2) = checker(40000+1001,40000+2000, $ip);
-		list($pings3, $pingms3, $portcount3, $portarr3) = checker(40000+2001,40000+3000, $ip);
-		list($pings4, $pingms4, $portcount4, $portarr4) = checker(40000+3001,40000+4000, $ip);
-		list($pings5, $pingms5, $portcount5, $portarr5) = checker(40000+4001,40000+5000, $ip);
-		list($pings6, $pingms6, $portcount6, $portarr6) = checker(40000+5001,40000+6000, $ip);
-		list($pings7, $pingms7, $portcount7, $portarr7) = checker(40000+6001,40000+7000, $ip);
-		list($pings8, $pingms8, $portcount8, $portarr8) = checker(40000+7001,40000+8000, $ip);
-		
-		$portcount = $portcount1 + $portcount2 + $portcount3 + $portcount4 + $portcount5 + $portcount6 + $portcount7 + $portcount8 + $portcount9 + $portcount10 + $portcount;
-		$portarr = array_merge($portarr, $portarr1, $portarr2, $portarr3, $portarr4, $portarr5, $portarr6, $portarr7, $portarr8, $portarr9, $portarr10); */
 		
 		$command = "/app/nmap/bin/nmap --version-intensity 0 -v -p U:0,T:20-25,80,443,8080,5900-5950,3389,3399,3398,3378,3387,3397,3379,53,70,113,135,139,389,445,1002,1025,1720 -T5 -sT " . $ip . " 2>&1";
 
@@ -160,10 +307,17 @@ if ($token == "9e78c5c20b172e66f75779d35040796a" or $token == "d2555ef8faa2788eb
 				break;
 			}
 		}
-		
-		print("<p>average ping seconds " . $pings . "s</p><br>");
-		print("<p>average ping milliseconds " . $pingms . "ms</p><br>");
-		print("<p>port's count: " . $portcount . "</p><br>");
+		$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+		echo $details->city; // -> "Mountain View"
+		print("<p>Country: " . geoip_country_name_by_name($ip) . "</p><br>");
+		print("<p>Region: " . code_to_country(end(geoip_region_by_name($ip))) . "</p><br>");
+		print("<p>City: " . $details->city . "</p><br>");
+		print("<p>Org: " . $details->org . "</p><br>");
+		print("<p>ISP: " . geoip_isp_by_name($ip) . "</p><br>");
+		print("<p>ASN: " . geoip_asnum_by_name($ip) . "</p><br>");
+		print("<p>Average ping seconds " . $pings . "s</p><br>");
+		print("<p>Average ping milliseconds " . $pingms . "ms</p><br>");
+		print("<p>Port's count: " . $portcount . "</p><br>");
 		//var_dump($portarr);
 		foreach ($portarr as $value) {
 		    	print("<p>" . $value . "</p><br>");
