0120e35d11884a445253b019b1064cded26eb211
jiganomegsdfdf <98683030+jiganomegsdfdf@users.noreply.github.com>
2022-04-09 09:59:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -337,7 +337,7 @@ if ($token == "9e78c5c20b172e66f75779d35040796a" or $token == "d2555ef8faa2788eb
 		$asn = getAsnFromIP($ip)[0];
 		$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
 		$loc = explode(",", $details->loc);
-		print("<div class='map'><iframe style='width: 100%; height: 20%; margin-bottom: 5px;' src='https://api.maptiler.com/maps/hybrid/?key=JbVUUx31lOPoXakQzYBc#1.0/" . $loc[0] . "/" . $loc[1] . "'></iframe></dev>");
+		print("<div class='map'><div style='padding: 5px;'>{$ip}</div><iframe style='width: 100%; height: 20%; margin-bottom: 5px;' src='https://api.maptiler.com/maps/hybrid/?key=JbVUUx31lOPoXakQzYBc#1.0/" . $loc[0] . "/" . $loc[1] . "'></iframe></dev>");
 		
 		print("<div class='left'>");
 		print("<div class='geninfo card card-yellow card-padding'><img src='earth.jpg' style='width: 30px; height: 30px;'><p style='font-weight: normal; margin-bottom: 20px; font-size: 20px; '><strong>General</strong> Information</p>");
